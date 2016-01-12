fa5360aded1b3edd40408fc7d3f64d38a33376b4
Will Melbourne <willmelbourne@gmail.com>
2016-01-12 11:38:16 +0000

    remove sourcemaps from autoprefixer from as causes conflict with sourcemaps from lessCSS

--- gulpfile.js
+++ gulpfile.js
@@ -58,12 +58,12 @@ gulp.task('watch-less', function() {
 
 gulp.task('autoprefixer', function () {
     return gulp.src('css/styles.css')
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(sourcemaps.write('.'))
+        // .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/'));
 });
 
